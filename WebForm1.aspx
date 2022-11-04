c56e4c798215d609b5105135415073aa95953519
nohambi <113665936+nohambi@users.noreply.github.com>
2022-11-04 12:45:36 +0200

    Add files via upload

---
+++ WebForm1.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div> <h1 style="text-align:center; background-color:burlywood;">Playlist</h1></div>
+        
+        <div style="margin-left: 0px;background-color:antiquewhite;" >
+            <asp:LinkButton ID="LinkButton1" runat="server" OnClick="LinkButton1_Click">Video 1</asp:LinkButton>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <b><asp:Label ID="Label1" runat="server" Text="بحالي ربي أعلم"></asp:Label></b>
+            <br />
+            <asp:LinkButton ID="LinkButton2" runat="server" OnClick="LinkButton2_Click">Video 2</asp:LinkButton>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <b><asp:Label ID="Label2" runat="server" Text="اهل المدينة"></asp:Label></b>
+            <br />
+            <asp:LinkButton ID="LinkButton3" runat="server" OnClick="LinkButton3_Click">Video 3</asp:LinkButton>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+           <b><asp:Label ID="Label3" runat="server" Text="جل الذي سواك"></asp:Label></b>
+            <br />
+            <asp:LinkButton ID="LinkButton4" runat="server" OnClick="LinkButton4_Click">Video 4</asp:LinkButton>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <b><asp:Label ID="Label4" runat="server" Text="انا جريح"></asp:Label></b>
+            <br />
+            <asp:LinkButton ID="LinkButton5" runat="server" OnClick="LinkButton5_Click">Video 5</asp:LinkButton>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <b><asp:Label ID="Label5" runat="server" Text="ابو عمر"></asp:Label></b>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Results;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Windows;
+using static System.Net.WebRequestMethods;
+using System.IO;
+using File = System.IO.File;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void LinkButton1_Click(object sender, EventArgs e)
+        {
+            string path = @"C:\Users\user\OneDrive\Desktop\TEST\TEST.txt";
+            File.Delete(path);
+            Response.Redirect("Http://youtu.be/PovR-HdO4zg");
+        }
+
+        protected void LinkButton2_Click(object sender, EventArgs e)
+        {
+            string path = @"C:\Users\user\OneDrive\Desktop\TEST\TEST.txt";
+            File.Delete(path);
+            Response.Redirect("https://youtu.be/4TAlZevsZ1U");
+        }
+
+        protected void LinkButton3_Click(object sender, EventArgs e)
+        {
+            string path = @"C:\Users\user\OneDrive\Desktop\TEST\TEST.txt";
+            File.Delete(path);
+            Response.Redirect("https://youtu.be/z-kqAZH60CY");
+        }
+
+        protected void LinkButton4_Click(object sender, EventArgs e)
+        {
+            string path = @"C:\Users\user\OneDrive\Desktop\TEST\TEST.txt";
+            File.Delete(path);
+            Response.Redirect("https://youtu.be/0ChIS1vnme4");
+        }
+
+        protected void LinkButton5_Click(object sender, EventArgs e)
+        {
+            string path = @"C:\Users\user\OneDrive\Desktop\TEST\TEST.txt";
+            File.Delete(path);
+            Response.Redirect("https://youtu.be/Jtn7xC1k1Xw");
+        }
+    }
+}
